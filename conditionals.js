79bff9880b904058f5afc813221f5dfe7b1f85bf
iroj <iroj@fresnoU.org>
2023-02-08 10:28:52 -0800

    conditional & inputs commit

---
+++ conditionals.js
@@ -0,0 +1,93 @@
+// Conditionals are if-statements
+let y = 7;
+
+if (y == 5){
+
+    console.log("it's 5!");
+
+}
+
+console.log("end of program");
+
+let z = 5;
+
+if (z == 5){
+
+    console.log("option1");
+
+} else {
+
+    console.log("option2");
+
+}
+
+console.log("end of program");
+
+let num2 = 0;
+
+if (num2 == 10){
+
+    console.log("equals 10");
+
+} else if (num2>5) {
+
+    console.log("greater than 5");
+
+} else{
+    console.log("some number")
+}
+let secret = 7;
+
+if (secret % 7 == 0){
+
+    console.log("divisible by 7");
+
+}
+
+if (secret > 10){
+
+    console.log("greater than 10");
+
+} else {
+
+    console.log("not greater than 10");
+
+}
+
+console.log("end of program");
+
+let x =5;
+if (x > 1) {
+    console.log("greater");
+} else {
+    console.log("not greater");
+}
+console.log("end of program");
+
+let c = 0;
+if (x > 10) {
+    console.log("greater");
+} else if (c < 5) {
+    console.log("not greater");
+}
+console.log("end of program");
+
+let password = "abc123";
+if (password == "abc123") {
+    console.log("Welcome!");
+} else if (password ==""){
+    console.log("Enter a password")
+} else {
+    console.log("invalid password")
+}
+console.log("end of program")
+
+let Password = "myGuess";
+if (Password == "abc123") {
+    console.log("Welcome!");
+} else if (Password ==""){
+    console.log("Enter a password")
+} else {
+    console.log("invalid password")
+}
+console.log("end of program")
