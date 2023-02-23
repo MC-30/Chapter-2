1f17e2c29a91eb8d0e12c40c01bf2d6c97a8cb4c
iroj <iroj@fresnoU.org>
2023-02-23 10:28:32 -0800

    forLoops1

---
+++ forLoops1.js
@@ -0,0 +1,45 @@
+// //1
+// for (let i = 0; i <=150; i++)
+// {
+//     console.log(i);
+// }
+//
+// //2
+// for (let i = 0; i <=40; i++)
+// {
+//     console.log(i**2);
+// }
+//
+// //3
+// for (let i = 0; i <=30; i++)
+// {
+//     console.log(i**3);
+// }
+
+// for (let i = 0; i <= 700; i++){
+//     if (i % 5 ==0) {
+//         console.log(i);
+//     }
+// }
+
+// for (let i = 350; i <= 1000; i++){
+//     if (i % 10 ==0) {
+//         console.log(i);
+//     }
+// }
+
+// let sum = 1
+// for (let i = 1; i <= 50; i++){
+//
+//         sum += i;
+// }
+// console.log(`sum: ${sum}`);
+
+let sum = 35
+for (let i = 35; i <= 70; i++){
+
+    sum += i;
+}
+console.log(`sum: ${sum}`);
+
+
