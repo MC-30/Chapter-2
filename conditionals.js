30153f6f29b5221f9dafce3839e81b10ad64922f
iroj <iroj@fresnoU.org>
2023-02-24 10:19:00 -0800

    forLoops1 updated

--- conditionals.js
+++ conditionals.js
@@ -1,93 +1,125 @@
 // Conditionals are if-statements
-let y = 7;
+// let y = 7;
+//
+// if (y == 5){
+//
+//     console.log("it's 5!");
+//
+// }
+//
+// console.log("end of program");
+//
+// let z = 5;
+//
+// if (z == 5){
+//
+//     console.log("option1");
+//
+// } else {
+//
+//     console.log("option2");
+//
+// }
+//
+// console.log("end of program");
+//
+// let num2 = 0;
+//
+// if (num2 == 10){
+//
+//     console.log("equals 10");
+//
+// } else if (num2>5) {
+//
+//     console.log("greater than 5");
+//
+// } else{
+//     console.log("some number")
+// }
+// let secret = 7;
+//
+// if (secret % 7 == 0){
+//
+//     console.log("divisible by 7");
+//
+// }
+//
+// if (secret > 10){
+//
+//     console.log("greater than 10");
+//
+// } else {
+//
+//     console.log("not greater than 10");
+//
+// }
+//
+// console.log("end of program");
+//
+// let x =5;
+// if (x > 1) {
+//     console.log("greater");
+// } else {
+//     console.log("not greater");
+// }
+// console.log("end of program");
+//
+// let c = 0;
+// if (x > 10) {
+//     console.log("greater");
+// } else if (c < 5) {
+//     console.log("not greater");
+// }
+// console.log("end of program");
+//
+// let password = "abc123";
+// if (password == "abc123") {
+//     console.log("Welcome!");
+// } else if (password ==""){
+//     console.log("Enter a password")
+// } else {
+//     console.log("invalid password")
+// }
+// console.log("end of program")
+//
+// let Password = "myGuess";
+// if (Password == "abc123") {
+//     console.log("Welcome!");
+// } else if (Password ==""){
+//     console.log("Enter a password")
+// } else {
+//     console.log("invalid password")
+// }
+// console.log("end of program")
 
-if (y == 5){
+// let x = 16;
+// 1f (x > 10) {
+//     console.log("greater");
+// }
+// if (x > 5) {
+//     console.log("not greater");
+// }
+// console. log("end of program")
 
-    console.log("it's 5!");
+// let x = 6;
+// if (x > 10) {
+//     console.log("greater");
+// }
+// if (x > 5) {
+//     console.log("not greater");
+//     console.log("end of program")
+// }
 
-}
 
-console.log("end of program");
+// let password = "abc123";
+// if (password == "abc123") {
+//     console.log("Welcome!");
+// }
+// if (password == ""){
+//     console.log("Enter a password")
+// } else {
+//     console.log("invalid password")
+// }
+// console.log("end of program")
 
-let z = 5;
-
-if (z == 5){
-
-    console.log("option1");
-
-} else {
-
-    console.log("option2");
-
-}
-
-console.log("end of program");
-
-let num2 = 0;
-
-if (num2 == 10){
-
-    console.log("equals 10");
-
-} else if (num2>5) {
-
-    console.log("greater than 5");
-
-} else{
-    console.log("some number")
-}
-let secret = 7;
-
-if (secret % 7 == 0){
-
-    console.log("divisible by 7");
-
-}
-
-if (secret > 10){
-
-    console.log("greater than 10");
-
-} else {
-
-    console.log("not greater than 10");
-
-}
-
-console.log("end of program");
-
-let x =5;
-if (x > 1) {
-    console.log("greater");
-} else {
-    console.log("not greater");
-}
-console.log("end of program");
-
-let c = 0;
-if (x > 10) {
-    console.log("greater");
-} else if (c < 5) {
-    console.log("not greater");
-}
-console.log("end of program");
-
-let password = "abc123";
-if (password == "abc123") {
-    console.log("Welcome!");
-} else if (password ==""){
-    console.log("Enter a password")
-} else {
-    console.log("invalid password")
-}
-console.log("end of program")
-
-let Password = "myGuess";
-if (Password == "abc123") {
-    console.log("Welcome!");
-} else if (Password ==""){
-    console.log("Enter a password")
-} else {
-    console.log("invalid password")
-}
-console.log("end of program")
+let length = 7
